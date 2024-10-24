f7684cc8891f5f25f450b31995167008d561c99a
lexafesyk <lexafesyk@users.noreply.github.com>
2024-10-24 21:27:51 +0000

    deploy: 693a315e235e49d38c021138e62e2c0d3b8f603a

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,13 +1,13 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
-        "tags": [],
-        "url": "/crispy-dollop/jekyll/update/2024/09/24/welcome-to-jekyll.html",
+        "title": "Post 1",
+        "excerpt":"Post 1 Header   Post 1 Content  ","categories": [],
+        "tags": ["tag1","tag2"],
+        "url": "/crispy-dollop/2024/09/24/post-1.html",
         "teaser": null
       },{
-        "title": "Post 2!",
-        "excerpt":"This is a Post 2 Header   This is a post 2 content  ","categories": ["misc"],
-        "tags": [],
-        "url": "/crispy-dollop/misc/2024/09/24/post-2.html",
+        "title": "Post 2",
+        "excerpt":"Post 2 Header   Post 2 Content  ","categories": [],
+        "tags": ["tag1","tag3"],
+        "url": "/crispy-dollop/2024/10/21/post-2.html",
         "teaser": null
       }]
